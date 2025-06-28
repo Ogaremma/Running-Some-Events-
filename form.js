9fa21e64371bb9e7cac9a638dd9b2aca7ade535f
Ogaremma <emmawills725@gmail.com>
2025-06-28 19:59:28 +0100

    Created some events with functions and addEventListener

--- form.js
+++ form.js
@@ -13,28 +13,42 @@ form.addEventListener('submit', function (e) {
     input.value = '';
 });
 
+
+
 // This code handles a tweet form submission, where it takes the input from the user and appends it to a list of tweets.
 // It also clears the input fields after submission.
 const tweetForm = document.querySelector('#tweetForm');
-// const usernameInput = document.querySelectorAll('input')[0];
-const tweetInput = document.querySelectorAll('input')[1];
 const order = document.querySelector('#tweetList');
 
 // This code adds an event listener to the tweet form to handle submission.
 // It prevents the default form submission behavior, creates a new list item with the tweet content,
 tweetForm.addEventListener('submit', function (e) {
+    // Prevent the default form submission behavior
     e.preventDefault();
-
     // Select the tweet list and the input values
-    // const username = usernameInput.value;
-    const tweet = tweetInput.value;
+    const usernameInput = tweetForm.elements.username;
+    const tweetInput = tweetForm.elements.tweet;
 
-    const newTweet = document.createElement('li');
+    addTweet(usernameInput.value, tweetInput.value);
 
-    newTweet.innerText = tweet;
-    // newTweet.innerText = username;
-    order.appendChild(newTweet);
-
+    // Clear the input fields
     usernameInput.value = '';
     tweetInput.value = '';
+});
+
+
+const addTweet = (username, tweet) => {
+    const newTweet = document.createElement('li');
+    const bTag = document.createElement('b');
+    bTag.append(username);
+    newTweet.append(bTag);
+
+    newTweet.append(`- ${tweet}`);
+    // Append the new tweet to the tweet list
+    order.append(newTweet);
+
+}
+
+order.addEventListener('click', function (e) {
+    e.target.nodeName === 'LI' && e.target.remove();
 });
